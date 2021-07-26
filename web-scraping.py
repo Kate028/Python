6357240d6000919f7b9490588b75a7450d0508ee
Kritika Ranjan <kritikaranjan28@gmail.com>
2021-07-26 12:42:16 +0530

    Update web-scraping.py

--- web-scraping.py
+++ web-scraping.py
@@ -1,14 +1,6 @@
 import requests
 import bs4
 from bs4 import BeautifulSoup
-
 page = requests.get('https://www.amazon.in/Raspberry-Pi-Model-RASP-PI-3-Motherboard/dp/B01CD5VC92')
 soup = BeautifulSoup(page.content, 'html.parser')
-
-rasp = soup.find(id = 'featurebullets_feature_div')
-
-items = rasp.find_all(class_ = 'a-list-item')
-
-print(items[0])
-# print(rasp)
-# print(items[0].find(class_= 'class="a-list-item"').get_text())
+print(soup.prettify())
