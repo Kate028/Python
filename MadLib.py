91113b77779c88af56d30a446efba59dd323eabf
Kritika Ranjan <kritikaranjan28@gmail.com>
2021-08-18 17:52:11 +0530

    Update MadLib.py

--- MadLib.py
+++ MadLib.py
@@ -1,31 +1,26 @@
 """Mab lib is one of the python projects for beginners.The input could be anything, an adjective, a noun, a pronoun, etc. 
 Once all the inputs are entered, the application will take the data and arrange the inputs into a story template form."""
 
-name = input("Enter your Name ")
-print(name)
-print("The Group of Frogs (Encouragement)")
+# concepts being used:
+# string concatenation
+# formatted output
+# value = "Pythonista" # some string variable
 
-print("As a group of frogs was traveling through the woods, two of them fell into a deep pit. 
+# string formatting
+# print("I am a " + value)
+# print("I am a {}".format(value))
+# print(f"I am a {value} ")
 
-When the other frogs crowded around the pit and saw how deep it was, they told the two frogs that there was no hope left for them.
+noun1 = input("What should I call you : ")
+noun2 = input("Who is your friend: ")
+adj = input("Give me an Adjective: ")
+verb1 = input("and a Verb: ")
+verb2 = input("one more Verb: ")
 
-However, the two frogs decided to ignore what the others were saying and they proceeded to try and jump out of the pit. 
+fav_hero = input("umm! Your Favorite Hero: ")
 
-Despite their efforts, the group of frogs at the top of the pit were still saying that they should just give up. That they would never make it out.
+madlib = f"I am {noun1} and I am a Pythonista. Python is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Keep coding and {verb2} like you are {fav_hero}.{noun2} too like it and we both used to code all the time."
+print(madlib)
 
-Eventually, one of the frogs took heed to what the others were saying and he gave up, falling down to his death. 
-
-The other frog continued to jump as hard as he could. Again, the crowd of frogs yelled at him to stop the pain and just die.
-
-He jumped even harder and finally made it out. When he got out, the other frogs said, “Did you not hear us?”
-
-The frog explained to them that he was deaf. He thought they were encouraging him the entire time.")
-
-
-color = input("Enter a color: ")
-pluralNoun = input("Enter a plural noun: ")
-celebrity = input("Enter a celebrity: ")
-
-print("Roses are", color)
-print(pluralNoun + " are blue")
-print("I love", celebrity)
+# you can create as long story as you want!
