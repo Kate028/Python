fefca05b86583c0e845f0882422b3aeb140f2a77
Mario Klug <mario.klug@sourcefactory.at>
2021-10-22 13:51:29 +0200

    moved qrgen.py to directory, added example output image and explanation to README.md

--- qrgen/qrgen.py
+++ qrgen/qrgen.py
@@ -9,21 +9,25 @@ import qrcode
 import sys
 
 def print_usage():
-    print("usage: %s data [output filename]" % sys.argv[0])
-    print(" if no output filename is set \"qrcode.png\" is used")
+    print("usage: %s url [output filename]" % sys.argv[0])
+    print(" if no output filename is set \"QR_code.png\" is used")
 
+# if no url is passed print usage text and exit
 if len(sys.argv) < 2:
     print_usage()
     sys.exit(1)
 
-data = sys.argv[1]
-output = "qrcode.png"
+url = sys.argv[1]
+output = "QR_code.png"
 
+# if 2nd parameter is set use this as filename instead of QR_code.png
 if len(sys.argv) > 2:
     output = sys.argv[2]
 
-img = qrcode.make(data)
+# create image object
+img = qrcode.make(url)
 
+# save image to file
 img.save(output)
 
 print("image saved as %s" % output)
